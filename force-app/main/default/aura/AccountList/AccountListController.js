1106e6f0d158b7c3dcf74fbbbad453161e0c3b87
Zachary Matthewstearn <zach.matthewstearn@kinliving.com>
2017-09-29 12:23:44 -0700

    add AccountsLoaded Event

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -2,7 +2,10 @@
     doInit : function(component, event) {
         var action = component.get("c.findAll");
         action.setCallback(this, function(a) {
-          component.set("v.accounts", a.getReturnValue()); 
+            component.set("v.accounts", a.getReturnValue());
+            var event = $A.get("e.c:AccountsLoaded");
+            event.setParams({"accounts": a.getReturnValue()});
+            event.fire();
         });
         $A.enqueueAction(action);
     }
